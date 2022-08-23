487037d71bd302a6f56f0b07e51d3149648c34d4
damaris <mdamarisnduku@gmail.com>
2022-08-23 13:44:08 +0300

    lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',30,'male','black','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',30,'male','black','choleric',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',31,'female','black','Melancolic',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Seegeant Brown',36,'male','black','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',36,'female','black','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',30,'male','black','phlegmatic',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',37,'female','black','sanguine',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,34,'male','black','choleric',1);
